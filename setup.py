cc66d7aeddb7ea7a5af13a69e5f4183890c42bf9
Viktor Ahlqvist <viktora@spotify.com>
2019-10-25 11:23:54 +0200

    Update the supported Python version and how testing/linting is done
    Updates the Tox file to test for all supported Python versions.
    Adds Black as a dependency and Black test as a step in Tox.
    
    Adds requirements-dev.txt file with the dev requirements since some
    was not explicity specified and some was only specified in tox.ini

--- setup.py
+++ setup.py
@@ -41,8 +41,8 @@ setuptools.setup(
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.8'
     ],
 )
