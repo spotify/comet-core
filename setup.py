8cd195117d7e54a90279bd55171aa785c1e91f97
Viktor Ahlqvist <viktora@spotify.com>
2019-10-30 13:02:43 +0100

    Fix the trove classifiers

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setuptools.setup(
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6"
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
 )
