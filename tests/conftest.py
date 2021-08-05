93e3af936438b70aaa2c0e1c26420e70781551b0
Viktor Ahlqvist <viktora@spotify.com>
2021-08-05 14:08:07 +0200

    Improve tests, especially around data store
    Re-define test utils as pytest fixtures for easier usage.
    
    Generalise pytest fixtures used for the data store and chain the
    fixtures so that the initialised data store-fixture depends on the data
    store-fixture etc. This will make it easier to do test-related changes
    in the objects.
    
    The fixtures can be improved further, there seems to be no clear
    consensus if the tests should create/load it's own data or if they
    should use the pre-populated fixtures. The current state is working, but
    the tests would be easier to read if they relied more on fixtures.
    
    Add docstrings to the data store tests, explaining what the tests do.
    
    Clean up some test code by removing duplicate tests, splitting some
    tests that do many things and remove unnecessary assert statements
    (e.g., first checking if the object is not None, then if it is a
    datetime instance. Only the datetime instance check is needed as it will
    then not be None).

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,29 +13,75 @@
 # limitations under the License.
 
 """Global test fixtures"""
-import os
+from datetime import datetime
+from typing import List
 
 import pytest
 
 from comet_core import Comet
+from comet_core.app import EventContainer
+from comet_core.data_store import DataStore
+from comet_core.model import EventRecord
+
+# pylint: disable=redefined-outer-name
 
 
 @pytest.fixture
-def app():
+def app() -> Comet:
+    """Returns a Comet app."""
     yield Comet()
 
 
 @pytest.fixture
-def test_db():
+def messages() -> List[EventContainer]:
+    """Get all test messages and their filenames as an iterator.
+
+    Returns:
+        EventContainer: some test event
+    """
+    event = EventContainer("test", {})
+    event.set_owner("test@acme.org")
+    event.set_fingerprint("test")
+    return [event]
+
+
+@pytest.fixture
+def data_store() -> DataStore:
+    """Creates a SQLite backed datastore."""
+    return DataStore("sqlite://")
+
+
+@pytest.fixture
+def test_db(messages, data_store) -> DataStore:
     """Setup a test database fixture
 
     Yields:
         DataStore: a sqlite backed datastore with all test data
     """
-    from comet_core.data_store import DataStore
-    from tests.utils import get_all_test_messages
 
-    data_store = DataStore("sqlite://")
-    for event in get_all_test_messages(parsed=True):
+    for event in messages:
         data_store.add_record(event.get_record())
+
+    yield data_store
+
+
+@pytest.fixture
+def data_store_with_test_events(data_store) -> DataStore:
+    """Creates a populated data store."""
+    one = EventRecord(received_at=datetime(2018, 7, 7, 9, 0, 0), source_type="datastoretest", owner="a", data={})
+    one.fingerprint = "f1"
+    two = EventRecord(received_at=datetime(2018, 7, 7, 9, 30, 0), source_type="datastoretest", owner="a", data={})
+    two.fingerprint = "f2"
+    three = EventRecord(
+        received_at=datetime(2018, 7, 7, 9, 0, 0),
+        source_type="datastoretest2",  # Note that this is another source type!
+        owner="b",
+        data={},
+    )
+    three.fingerprint = "f3"
+
+    data_store.add_record(one)
+    data_store.add_record(two)
+    data_store.add_record(three)
+
     yield data_store
